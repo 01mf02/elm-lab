99ac0c3784d425222e30a3914546967e1e574d60
Michael Färber <01mf02@gmail.com>
2019-01-29 05:54:52 +0100

    Use original Background drawing module.

--- src/View/Background.elm
+++ src/View/Background.elm
@@ -5,21 +5,22 @@ import Svg exposing (Svg)
 import Svg.Attributes as SA
 import Svg.Events as SE
 
-import Geometry.Svg as Svg
+import BoundingBox2d
 import Point2d
-import Rectangle2d
+import Geometry.Svg as Svg
 
 import Entity exposing (EntityId)
-import Pointer exposing (Msg(..))
+import Pointer
+
 
 draw : EntityId -> Svg Pointer.Msg
 draw id =
   let
-    rect = Rectangle2d.from Point2d.origin (Point2d.fromCoordinates ( 800, 600 ))
     attributes =
-     [ SE.on "click" <| JD.map (Pointer.Clicked id) <| Pointer.pageCoordDecoder
-     , SE.on "mousemove" <| JD.map (Pointer.MouseMoved id) <| Pointer.pageCoordDecoder
-     , SA.id "background"
-     ]
+      [ SE.on "click" <| JD.map (Pointer.Clicked id) <| Pointer.pageCoordDecoder
+      , SE.on "mousemove" <| JD.map (Pointer.MouseMoved id) <| Pointer.pageCoordDecoder
+      , SA.id "background"
+      ]
+    box = BoundingBox2d.from Point2d.origin (Point2d.fromCoordinates (800, 600))
   in
-  Rectangle2d.toPolygon rect |> Svg.polygon2d attributes
+  Svg.boundingBox2d attributes box
